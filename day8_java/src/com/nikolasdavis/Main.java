979371f8d52916d373332232a5b8d721a5f42805
Nikolas Davis <ndavis@newrelic.com>
2016-01-03 10:27:44 -0800

    Finished day 8, part 2 using StringBuilder.

--- day8_java/src/com/nikolasdavis/Main.java
+++ day8_java/src/com/nikolasdavis/Main.java
@@ -5,7 +5,6 @@ import com.nikolasdavis.StringParser;
 public class Main {
 
     public static void main(String[] args) {
-        //StringParser.lineParser("./input.txt");
-        StringParser.parseString("\"aaa\\\\aaa\"");
+        StringParser.lineParser("./input.txt", false);
     }
 }
--- day8_java/src/com/nikolasdavis/StringParser.java
+++ day8_java/src/com/nikolasdavis/StringParser.java
@@ -13,22 +13,31 @@ import com.nikolasdavis.Constants;
  * Created by ndavis on 1/1/16.
  */
 public class StringParser {
-    public static String[] lineParser(String fileName) {
+
+    public static String[] lineParser(String fileName, boolean decode) {
+        int debugDiff = 0;
         try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
             String line;
             while ((line = br.readLine()) != null) {
-                System.out.println(line.length());
-                break;
+                if(decode) {
+                    debugDiff = debugDiff + (line.length() - decodeString(line).length());
+                } else {
+                    debugDiff = debugDiff + (encodeString(line).length() - line.length());
+                }
             }
         } catch (FileNotFoundException ex) {
-            System.out.println("File not found.");
+            System.out.println("StringParser: File not found.");
         } catch (IOException ex) {
-            System.out.println("Encountered IO error.");
+            System.out.println("StringParser: Encountered IO error.");
+        }
+        if(Constants.DEBUG) {
+            System.out.println("StringParser: Difference of " + debugDiff + " chars detected.");
         }
         return new String[] {"haha", "notreadyyet"};
     }
 
-    public static String parseString(String string) {
+    // Limited to 256 characters of output
+    public static String decodeString(String string) {
         String stringOut;
         // I've since learned I could use a StringBuilder to build the output string. This is harder.
         char[] buffer = new char[256];
@@ -37,7 +46,7 @@ public class StringParser {
         for(int i = 1; i < string.length() - 1; i++) {
             char newChar = 0;
             if(Constants.DEBUG) {
-                System.out.println("Backslash found at: " + i);
+                System.out.println("StringParser.parseString: Backslash found at: " + i);
             }
             if(string.charAt(i) == '\\') {
                 char type = string.charAt(i+1);
@@ -49,7 +58,7 @@ public class StringParser {
                     i++;
                 } else if(type == 'x') {
                     // Parse hexadecimal character
-                    newChar = (char)Integer.parseInt(string.substring(i+2, i+3));
+                    newChar = (char)Integer.parseInt(string.substring(i+2, i+3), 16);
                     i += 3;
                 }
             } else {
@@ -61,9 +70,34 @@ public class StringParser {
 
         stringOut = new String(buffer, 0, index);
         if(Constants.DEBUG) {
-            System.out.println("Input string (" + string + ") length: " + string.length());
-            System.out.println("Output string (" + stringOut + ") length: " + stringOut.length());
+            System.out.println("StringParser.parseString: Input string (" + string + ") length: " + string.length());
+            System.out.println("StringParser.parseString: Output string (" + stringOut + ") length: " + stringOut.length());
         }
         return stringOut;
+    }
+
+    // Uses StringBuilder .. max value of Integer.MAX_VALUE (openjdk)
+    public static String encodeString(String string) {
+        StringBuilder stringOut = new StringBuilder();
+        stringOut.append('"'); // First quote
+        for(int i = 0; i < string.length(); i++) {
+            if(Constants.DEBUG) {
+                System.out.println("StringParser.parseString: Backslash found at: " + i);
+            }
+            if(string.charAt(i) == '\\') {
+                stringOut.append(new String("\\\\"));
+            } else if (string.charAt(i) == '"') {
+                stringOut.append(new String("\\\""));
+            } else {
+                stringOut.append(string.charAt(i));
+            }
+        }
+        stringOut.append('"'); // Second quote
+
+        if(Constants.DEBUG) {
+            System.out.println("StringParser.parseString: Input string (" + string + ") length: " + string.length());
+            System.out.println("StringParser.parseString: Output string (" + stringOut + ") length: " + stringOut.length());
+        }
+        return stringOut.toString();
     }
 }
